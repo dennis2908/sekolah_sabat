d52666f578c36cd1b14a2fe278d8f5793af93715
dennis2908 <manullang_d@yahoo.com>
2024-06-15 18:25:36 +0700

    kebenaran-sabat

---
+++ app/Http/Controllers/KebenaranSabatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class KebenaranSabatController extends Controller
+{
+
+
+    public function index()
+    {
+        return view('kebenaran-sabat.kebenaran-sabat');
+    }
+}
